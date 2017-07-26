3adf2eab83d0ea18459ea8fc44904a9525b2cba6
Maurizio Cescon <Maurizio.CESCON@ext.ec.europa.eu>
2017-07-26 11:28:42 +0200

    bug fixes

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -6,7 +6,7 @@ import rootReducer from "../reducers";
 const configureStore = preloadedState => createStore(
   rootReducer,
   preloadedState,
-  applyMiddleware(thunk, api)
+  // applyMiddleware(thunk, api)
 );
 
 export default configureStore;
