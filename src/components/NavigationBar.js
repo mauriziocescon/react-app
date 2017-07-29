253b38f90ae4d91acfeb4de53a3246300cc6e170
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-07-29 14:59:59 +0200

    navigationbar tests

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, IndexLink } from "react-router";
+import { Link } from "react-router";
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 import { FormattedMessage } from "react-intl";
 import "./NavigationBar.css";
--- src/components/NavigationBar.test.js
+++ src/components/NavigationBar.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import NavigationBar from "./NavigationBar";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App/>, div);
+  ReactDOM.render(<NavigationBar/>, div);
 });
