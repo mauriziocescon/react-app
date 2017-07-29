155cc302cf80e3945a2e03e94eb13dffba17d144
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-07-29 16:51:05 +0200

    Improve navigationbar component

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router";
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 import { FormattedMessage } from "react-intl";
 import "./NavigationBar.css";
 
 class NavigationBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.dropdownTitle = this.props.selectedLanguage;
+    this.languages = this.props.availableLanguages;
+
+    this.createLanguagesItems();
+  }
+
+  createLanguagesItems() {
+    this.languagesItems = this.languages.map((language) => {
+      return (
+        <MenuItem key={language} eventKey={language}>{language}</MenuItem>
+      )
+    });
+  }
+
+  handleSelect(eventKey, event) {
+    this.props.onLanguageChange(eventKey, event);
+  }
+
   render() {
     return (
       <Navbar fluid fixedTop default collapseOnSelect>
@@ -21,7 +44,7 @@ class NavigationBar extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={2} href="#">
+            <NavItem eventKey={1} href="#">
               <Link to="albums" activeClassName="active" className="NavigatorBarLink">
                 <FormattedMessage
                   id="navigationbar.albums"
@@ -29,7 +52,7 @@ class NavigationBar extends Component {
                 />
               </Link>
             </NavItem>
-            <NavItem eventKey={3} href="#">
+            <NavItem eventKey={2} href="#">
               <Link to="users" activeClassName="active" className="NavigatorBarLink">
                 <FormattedMessage
                   id="navigationbar.users"
@@ -39,9 +62,8 @@ class NavigationBar extends Component {
             </NavItem>
           </Nav>
           <Nav pullRight>
-            <NavDropdown eventKey={4} title="Language" id="basic-nav-dropdown">
-              <MenuItem eventKey={4.1}>en</MenuItem>
-              <MenuItem eventKey={4.2}>it</MenuItem>
+            <NavDropdown eventKey={3} title={this.dropdownTitle} onSelect={this.handleSelect} id="basic-nav-dropdown">
+              {this.languagesItems}
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
@@ -49,5 +71,11 @@ class NavigationBar extends Component {
     );
   }
 }
+
+NavigationBar.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  availableLanguages: PropTypes.array.isRequired,
+  onLanguageChange: PropTypes.func.isRequired
+};
 
 export default NavigationBar;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,10 +5,31 @@ import "./App.css";
 import NavigationBar from "../../components/NavigationBar";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // todo: move values in some constants
+    this.selectedLanguage = "en";
+    this.availableLanguages = ["en", "it"];
+  }
+
+  handleLanguageChange(eventKey, event) {
+    // todo call the store
+    // this.setState({});
+
+    console.log(eventKey, event);
+
+    // creates a FormattedMessage
+    // this.dropdownTitle = <FormattedMessage id={this.props.selectedLanguage} defaultMessage="Languages"/>;
+  }
+
   render() {
     return (
       <div className="App">
-        <NavigationBar/>
+        <NavigationBar
+          selectedLanguage={this.selectedLanguage}
+          availableLanguages={this.availableLanguages}
+          onLanguageChange={this.handleLanguageChange}/>
         <div className="App-main-content">
           {this.props.children}
         </div>
