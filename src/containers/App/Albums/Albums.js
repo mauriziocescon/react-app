bb302c684beba21ad69da9f3691a667c14c44486
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-07-31 23:18:55 +0200

    minor changes

--- src/containers/App/Albums/Albums.js
+++ src/containers/App/Albums/Albums.js
@@ -2,33 +2,28 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { FormattedMessage, FormattedNumber } from "react-intl";
-import { Button } from "react-bootstrap";
+import { Grid, Row, Col, Button } from "react-bootstrap";
 
 import "./Albums.css";
 
 class Albums extends Component {
-
   render() {
     return (
-      <div className="container-fluid Albums">
-        <div className="row">
-          <div className="col-xs-12">
-            <div>
-              <div>
-                <FormattedNumber value={1000}/>
-              </div>
-              <div>
-                <Button bsStyle="primary">
-                  <FormattedMessage
-                    id="albums.example"
-                    defaultMessage="default message"
-                  />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Grid fluid className="Albums">
+        <Row>
+          <Col xs={12}>
+            <FormattedNumber value={1000}/>
+          </Col>
+          <Col xs={12}>
+            <Button bsStyle="primary">
+              <FormattedMessage
+                id="albums.example"
+                defaultMessage="default message"
+              />
+            </Button>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
--- src/containers/App/Users/Users.js
+++ src/containers/App/Users/Users.js
@@ -2,48 +2,43 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { FormattedMessage, FormattedNumber } from "react-intl";
-import { Button } from "react-bootstrap";
+import { Grid, Row, Col, Button } from "react-bootstrap";
 
 import "./Users.css";
 
 class Users extends Component {
-
   render() {
     return (
-      <div className="container-fluid Users">
-        <div className="row">
-          <div className="col-xs-12">
-            <div>
-              <div>
-                <FormattedNumber value={1000}/>
-              </div>
-              <div>
-                <FormattedMessage
-                  id="users.example"
-                  defaultMessage="default message"
-                />
-              </div>
-              <div>
-                <FormattedMessage
-                  id="users.example2"
-                  defaultMessage="default message"
-                  values={{
-                    value: "passed message"
-                  }}
-                />
-              </div>
-              <div>
-                <Button bsStyle="primary">
-                  <FormattedMessage
-                    id="users.example"
-                    defaultMessage="default message"
-                  />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Grid fluid className="Albums">
+        <Row>
+          <Col xs={12}>
+            <FormattedNumber value={1000}/>
+          </Col>
+          <Col xs={12}>
+            <FormattedMessage
+              id="users.example"
+              defaultMessage="default message"
+            />
+          </Col>
+          <Col xs={12}>
+            <FormattedMessage
+              id="users.example2"
+              defaultMessage="default message"
+              values={{
+                value: "passed message"
+              }}
+            />
+          </Col>
+          <Col xs={12}>
+            <Button bsStyle="primary">
+              <FormattedMessage
+                id="albums.example"
+                defaultMessage="default message"
+              />
+            </Button>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
