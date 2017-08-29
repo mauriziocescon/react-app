18406f3f926d570794b11f52e17df98a13d72f32
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-08-29 23:53:54 +0200

    minor changes

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,8 +1,16 @@
 import { fetchUsersUrl } from "../constants";
 
 export function fetchUsers(textSearch) {
-  return fetch(fetchUsersUrl + (textSearch ? "?q=" + textSearch : ""))
+  const options = {
+    method: "GET",
+    headers: {},
+    // body: params,
+    // credentials: "omit"
+  };
+
+  return fetch(`${fetchUsersUrl}?q=${textSearch || ""}`, options)
     .then(response => {
       return response.status === 200 ? response.json() : Promise.reject("Error");
-    });
+    })
+    .catch(error => Promise.reject(error));
 }
