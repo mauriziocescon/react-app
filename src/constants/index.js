6f1263a5eca3fe27c072917f9899bda726aab38b
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-11-06 00:43:49 +0100

    https

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 // URLs
-export const fetchAlbumsUrl = "http://jsonplaceholder.typicode.com/albums";
-export const fetchCommentsUrl = "http://jsonplaceholder.typicode.com/comments";
-export const fetchPhotosUrl = "http://jsonplaceholder.typicode.com/photos";
-export const fetchPostsUrl = "http://jsonplaceholder.typicode.com/posts";
-export const fetchTodosUrl = "http://jsonplaceholder.typicode.com/todos";
-export const fetchUsersUrl = "http://jsonplaceholder.typicode.com/users";
+export const fetchAlbumsUrl = "https://jsonplaceholder.typicode.com/albums";
+export const fetchCommentsUrl = "https://jsonplaceholder.typicode.com/comments";
+export const fetchPhotosUrl = "https://jsonplaceholder.typicode.com/photos";
+export const fetchPostsUrl = "https://jsonplaceholder.typicode.com/posts";
+export const fetchTodosUrl = "https://jsonplaceholder.typicode.com/todos";
+export const fetchUsersUrl = "https://jsonplaceholder.typicode.com/users";
