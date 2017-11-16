cb9fa5e8c16ad2be1fbe881fbeeea8ae4a476d99
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-11-16 18:41:28 +0100

    minor changes

--- mock-server/server.js
+++ mock-server/server.js
@@ -1,20 +1,30 @@
 const path = require("path");
 const jsonServer = require("json-server");
-const server = jsonServer.create();
+const app = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "./db.json"));
 const middlewares = jsonServer.defaults({
-  static: "build"
+  static: "build",
 });
 
 // Add middlewares
-server.use(middlewares);
+app.use(middlewares);
 
-// Simulate a server side error response
-server.use((req, res, next) => {
+// Simulate server side delay
+app.use((req, res, next) => {
+  const randomOutcome = Math.random();
+  if (randomOutcome < 0.5) {
+    setTimeout(next, Math.floor(( Math.random() * 10000 ) + 100));
+  } else {
+    next();
+  }
+});
+
+// Simulate server side errors
+app.use((req, res, next) => {
   const randomOutcome = Math.random();
   if (randomOutcome < 0.1 && req.path.startsWith("/api/")) {
     res.status(500).jsonp({
-      error: "Error message here"
+      error: "Error message here",
     });
   } else {
     // Continue to JSON Server router
@@ -23,15 +33,15 @@ server.use((req, res, next) => {
 });
 
 // Mount the router based on db.json
-server.use("/api", router);
+app.use("/api", router);
 
 // Fallback on frontend routes
-server.get("*", (req, res) => {
+app.get("*", (req, res) => {
   // load the single view file (frontend will handle the page changes on the front-end)
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 // Start listening
-server.listen(5000, () => {
+app.listen(5000, () => {
   console.log("JSON Server is running! Open the browser at http://localhost:5000");
 });
